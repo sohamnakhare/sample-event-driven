b89529ec880b15fe38f4fb600754eb804ff7aaaf
Soham Nakhare <soham.nakhare@shippit.com>
2024-07-28 16:04:54 +0530

    add hello world path

--- handler.js
+++ handler.js
@@ -14,6 +14,12 @@ app.get("/hello", (req, res, next) => {
   });
 });
 
+app.get("/hello-world", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+
 app.use((req, res, next) => {
   return res.status(404).json({
     error: "Not Found",
