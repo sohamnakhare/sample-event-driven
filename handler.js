141ac6a36b848609680ddfe831f07bec9ff1222c
Soham Nakhare <soham.nakhare@shippit.com>
2024-08-12 18:25:21 +0530

    fix module error

--- handler.js
+++ handler.js
@@ -1,9 +1,9 @@
 const serverless = require("serverless-http");
 const express = require("express");
-import { SQS } from '@aws-sdk/client-sqs';
+const awsSdk = require('@aws-sdk/client-sqs');
 const app = express();
 
-const sqs = new SQS({region: 'us-east-1'});
+const sqs = new awsSdk.SQS({region: 'us-east-1'});
 
 app.get("/", (req, res, next) => {
   return res.status(200).json({
