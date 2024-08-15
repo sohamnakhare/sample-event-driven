2d0810064c2c35e4583065f1a814188e5c80367e
Soham Nakhare <soham.nakhare@shippit.com>
2024-08-15 14:37:32 +0530

    add secrets

--- handler.js
+++ handler.js
@@ -10,6 +10,8 @@ const AWS = require('aws-sdk');
 // Initialize the S3 client
 const s3 = new AWS.S3({
     region: 'us-east-1', // Specify your region
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
 
