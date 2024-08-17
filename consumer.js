01b4911055c6bf80a580f72e0d533910364c3df2
Soham Nakhare <soham.nakhare@shippit.com>
2024-08-17 15:13:01 +0530

    sqs throw error

--- consumer.js
+++ consumer.js
@@ -4,30 +4,7 @@ const authToken = process.env.TWI_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
 exports.handler = async (event) => {
-  console.log('Received SQS event:', JSON.stringify(event, null, 2));
-  // Iterate over each record in the SQS event
-  for (const record of event.Records) {
-    // Process each SQS message
-    try {
-      const messageBody = JSON.parse(record.body);
-      console.log('Processing message:', messageBody);
-
-      // Example processing logic
-      // You can replace this with your actual processing logic
-      await processMessage(messageBody);
-
-      // Successfully processed, no need to delete the message explicitly,
-      // SQS will automatically remove the message if the function succeeds
-    } catch (error) {
-      console.error('Error processing message:', error);
-      // Optionally handle errors here, e.g., send the message to a dead-letter queue
-    }
-  }
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Messages processed successfully!' }),
-  };
+  throw new Error('error');
 };
 
 // Example function to process the message
